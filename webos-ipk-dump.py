c80459bd0eadd6e58874e29eb5f039c0ad312f87
PrplHaz4 <prplhaz4444@hotmail.com>
2014-12-22 12:58:57 -0500

    handle windows UTF-8 encoding

--- webos-ipk-dump.py
+++ webos-ipk-dump.py
@@ -45,30 +45,50 @@ def downloadIpk(token, deviceId, ipkUrl):
     headers = getRequestHeaders(token, deviceId)
 
     print(appFilename, end='')
+    sys.stdout.flush()
     if not os.path.exists(".\\ipk\\" + appFilename):
         r = urllib.request.Request(ipkUrl, None, headers)
         try:
             with urllib.request.urlopen(r, None) as response, open(".\\ipk\\" + appFilename, 'wb') as out_file:
                 shutil.copyfileobj(response, out_file)
             print(" ....saved!")
-        except urllib.error.HTTPError:
+        except urllib.error.URLError:
             print(" ....Error downloading file")
+            print(ipkUrl)
+            e = sys.exc_info()[0]
+            print(e)
     else:
         print(" ...already exists.")
 
 def readIpkJson(inputFilename):
+    f = None
     try:
-        f = json.load(codecs.open(inputFilename, 'r', 'utf8'))
+        print("Trying UTF-8-sig...")
+        sys.stdout.flush()
+        f = json.load(codecs.open(inputFilename, 'r', 'utf_8_sig'))
+        return f
     except:
         e = sys.exc_info()[0]
         print(e)
-        print("...trying UTF-16...")
-        try:
-            f = json.load(codecs.open(inputFilename, 'r', 'utf16'))
-        except:
-            e = sys.exc_info()[0]
-            print(e)
-    return f
+    try:
+        print("Trying UTF-8...")
+        sys.stdout.flush()
+        f = json.load(codecs.open(inputFilename, 'r', 'utf8'))
+        return f
+    except:
+        e = sys.exc_info()[0]
+        print(e)
+    try:
+        print("Trying UTF-16...")
+        sys.stdout.flush()
+        f = json.load(codecs.open(inputFilename, 'r', 'utf16'))
+        return f
+    except:
+        e = sys.exc_info()[0]
+        print(e)
+
+    print("\nCan't read input file!  Try saving it with UTF-8 encoding.")
+    sys.exit()
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
